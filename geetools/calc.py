d468bcfb823faec4c2fd5baeca86464c65484653
Guillermo Barrios del Valle <gbv@ier.unam.mx>
2022-04-14 12:05:54 -0500

    Update calc.py

--- geetools/calc.py
+++ geetools/calc.py
@@ -8,6 +8,6 @@ def wd(df):
     df['vgeo'] = -df.u
     df['ws'] = (df.ugeo**2 + df.vgeo**2 + df.w**2)**0.5
     df['wd'] = 270 + (np.arctan2(df.vgeo,df.ugeo)*DperR)
-    df.wd    = a1.wd%360.
+    df.wd    = df.wd%360.
     return df[['u','v','w','ws','wd']]
 
