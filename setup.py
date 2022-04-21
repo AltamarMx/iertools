1620d5295269905763683a2941ca1d441303224f
Guillermo Barrios <gbv@ier.unam.mx>
2022-04-21 10:55:10 -0500

    sun path correccion de nombres

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='geetools',
-    version='0.3.0',
+    version='0.3.1',
     author='Guillermo Barrios',
     author_email='gbv@ier.unam.mx',
     description='New Package fitted for Energy Building Group (GEE)',
